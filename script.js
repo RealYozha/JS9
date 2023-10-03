2d7984b4600898015c0378190dab7a119e7450f2
RealYozha <ItzYozha@ya.ru>
2023-10-03 18:18:20 +0300

    1.0

---
+++ script.js
@@ -0,0 +1,51 @@
+function sayHey(name) {
+    console.log("Привет,", name)
+}
+
+function showAboutPC(CPU, RAM, GPU = "Нет") {
+    console.log("У тебя стоит процессор:", CPU,
+        "| Видеокарта: ", GPU,
+        "| Оперативная память:", RAM);
+};
+
+function temperature() {
+    let goodTemp = 0;
+    let badTemp = 0;
+    let timerId = setInterval(() => {
+        let temp = Math.random() * (100 - 35) + 35;
+        if (temp < 80) {
+            goodTemp++;
+        } else {
+            badTemp++;
+        }
+    }, 1000);
+    setTimeout(() => {
+        clearInterval(timerId);
+        goodTemp > badTemp ? console.log("Всё хорошо") : console.log("Есть неполадки")
+    }, 20000);
+};
+
+function undervolt() {
+    let voltage = 1;
+    let frequency = 1900;
+    let gpuTemperature = 70;
+    let variant = 1;
+    while (variant > 0) {
+        variant = Math.floor(Math.random() * (3 - 0 + 1)) + 0;
+        voltage -= 0.025;
+        gpuTemperature -= 2;
+    }
+    console.log("При андервольтинге: Вольтаж -", voltage, "| Температура:", gpuTemperature, "| Частота:", frequency);
+};
+
+function clear() {
+    setTimeout(() => console.log("Мы почистили и заменили термопасту за 5 секунд, обращайтесь"), 5000);
+};
+
+sayHey("Саша")
+showAboutPC("Intel Core i5-3450", "4GB x2", "GIGABYTE Raedon RX 570");
+temperature();
+undervolt();
+if (confirm("Хотите почистим компютер?")) {
+    clear();
+}
